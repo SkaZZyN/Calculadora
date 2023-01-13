40d57698c18af60a97489fd7ea2fb0e246500b0b
Neto Santos <hunaldoneto21@gmail.com>
2023-01-13 05:45:30 -0300

    Atualizado

---
+++ assets/js/app.js
@@ -0,0 +1,130 @@
+function criaCalculadora(){
+
+    return {
+
+        display: document.querySelector('.display'),
+
+        inicia(){
+
+            this.cliqueBotoes();
+            this.pressionaBackSpace();
+            this.pressionaEnter();
+
+        },
+
+        cliqueBotoes(){
+
+            document.addEventListener('click', e => {
+
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')){
+
+                    this.btnParaDisplay(el.innerText);
+
+                }
+
+                if(el.classList.contains('btn-clear')){
+
+                    this.clearDisplay();
+
+                }
+
+                if(el.classList.contains('btn-del')){
+
+                    this.deleteOne();
+
+                }
+
+                if(el.classList.contains('btn-eq')){
+
+                    this.realizaConta();
+
+                }
+
+                this.display.focus();//Mandar o foco ao display a cada botão apertado
+
+            }); //ou usar bind(this)
+
+        },
+
+
+        realizaConta(){
+
+            let conta = this.display.value;
+
+            try{
+
+                conta = eval(conta);
+
+                if(!conta){
+                    
+                    alert('Conta inválida');
+                    return;
+
+                }
+
+                this.display.value = String(conta);
+
+            }catch (e){
+
+                alert("Conta Invalida!");
+                return;
+
+            }
+
+        },
+
+        btnParaDisplay(valor){
+
+            this.display.value += valor;
+
+        },
+
+        clearDisplay(){
+
+            this.display.value = "";
+
+        },
+
+        deleteOne(){
+
+            this.display.value = this.display.value.slice(0, -1);
+
+        },
+
+        pressionaBackSpace() {
+
+            this.display.addEventListener('keydown', e => {
+
+              if (e.keyCode === 8) {
+
+                e.preventDefault();
+                this.clearDisplay();
+
+              }
+
+            });
+
+        },
+      
+        pressionaEnter() {
+
+            this.display.addEventListener('keyup', e => {
+
+              if (e.keyCode === 13) {
+
+                this.realizaConta();
+
+              }
+
+            });
+
+        },
+
+    };
+
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
